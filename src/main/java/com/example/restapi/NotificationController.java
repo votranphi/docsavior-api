071708c813280ca6b7587773ef0bf6b784dea08e
Vo Tran Phi <songokupkj@gmail.com>
2024-06-21 02:13:50 +0700

    refactor: remove unncessary class

--- src/main/java/com/example/restapi/NotificationController.java
+++ src/main/java/com/example/restapi/NotificationController.java
@@ -8,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.core.util.Json;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
