bd2ae012b63051a951f9ebeb2a28abe25b737baa
Vo Tran Phi <songokupkj@gmail.com>
2024-06-22 21:55:39 +0700

    feat: lookup post api

--- src/main/java/com/example/restapi/NewsfeedController.java
+++ src/main/java/com/example/restapi/NewsfeedController.java
@@ -1,6 +1,10 @@
 package com.example.restapi;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,6 +94,64 @@ public class NewsfeedController {
         return ResponseEntity.ok(jsonArray.toString());
     }
     
+    @PostMapping("/look_up")
+    public ResponseEntity<?> postLookUpPost(@RequestParam String lookUpInfo) {
+        // get all users in database
+        List<Newsfeed> newsfeeds = newsfeedService.getAllPosts();
+
+        // lowercase the lookUpInfo and remove the leading/trailing space if it has
+        String lowercasedLKI = lookUpInfo.toLowerCase().trim();
+
+        // split the string to do the searching
+        String[] spl = lowercasedLKI.split("\s");
+
+        // init the foundUsers which stores found users
+        List<Newsfeed> foundNewsfeeds = new ArrayList<>();
+
+        // found newsfeed
+        for (Newsfeed i : newsfeeds) {
+            for (String j : spl) {
+                // add the newfeed if it's contains at least one word of the lookUpInfo
+                if (
+                    i.getPostDescription().toLowerCase().contains(j) ||
+                    i.getPostContent().toLowerCase().contains(j) ||
+                    i.getFileName().toLowerCase().contains(j) ||
+                    i.getFileExtension().toLowerCase().contains(j)
+                ) {
+                    foundNewsfeeds.add(i);
+                    break;
+                }
+            }
+        }
+
+        // init a jsonObject
+        JSONObject jsonObject = new JSONObject();
+
+        // init a jsonArray
+        JSONArray jsonArray = new JSONArray();
+
+        // convert foundUsers to jsonArray
+        for (Newsfeed i : foundNewsfeeds) {
+            JSONObject temp = new JSONObject();
+            jsonObject.put("id", i.getId());
+            jsonObject.put("username", i.getUsername());
+            jsonObject.put("postDescription", i.getPostDescription());
+            jsonObject.put("postContent", i.getPostContent());
+            jsonObject.put("likeNumber", i.getLikeNumber());
+            jsonObject.put("dislikeNumber", i.getDislikeNumber());
+            jsonObject.put("commentNumber", i.getCommentNumber());
+            jsonObject.put("fileData", i.getFileData());
+            jsonObject.put("fileName", i.getFileName());
+            jsonObject.put("fileExtension", i.getFileExtension());
+
+            jsonArray.put(temp);
+        }
+
+        // put the jsonArray to jsonObject
+        jsonObject.put("foundNewsfeeds", jsonArray);
+
+        return ResponseEntity.ok(jsonObject.toString());
+    }
 
     @PostMapping("/like")
     public ResponseEntity<?> postLike(@RequestParam Integer id)
